97078f0265c077301738c8585a0b383f6a67b019
Ridrog <michael@ridrog.de>
2017-03-06 22:01:33 +0100

    Cleanup Views

--- src/Console/Views/home.blade.php
+++ src/Console/Views/home.blade.php
@@ -40,12 +40,9 @@
     <nav class="tabs is-boxed is-fullwidth">
         <div class="container">
             <ul>
-                <li class="is-active"><a>Overview</a></li>
-                <li><a>Modifiers</a></li>
-                <li><a>Grid</a></li>
-                <li><a>Elements</a></li>
-                <li><a>Components</a></li>
-                <li><a>Layout</a></li>
+                <li class="is-active"><a>Link active</a></li>
+                <li><a>Link</a></li>
+
             </ul>
         </div>
     </nav>
